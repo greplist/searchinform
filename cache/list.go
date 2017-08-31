deefed9f035ea76ca1de83f65992d3ca497aaa97
Anton Grabko <anton.grabko@synesis.ru>
2017-08-31 11:57:01 +0300

    cache: list: first version

--- cache/list.go
+++ cache/list.go
@@ -16,8 +16,12 @@ type node struct {
 	value listValue
 }
 
+func loadNode(ptr *unsafe.Pointer) *node {
+	return (*node)(atomic.LoadPointer(ptr))
+}
+
 func (n *node) Next() *node {
-	return (*node)(atomic.LoadPointer(&n.next))
+	return loadNode(&n.next)
 }
 
 // list - lock-free list for Hash map
@@ -29,7 +33,47 @@ func (l *list) Head() *node {
 	return (*node)(atomic.LoadPointer(&l.head))
 }
 
+func find(start *node, key string) *node {
+	for this := start; this != nil; this = this.Next() {
+		if this.key == key {
+			return this
+		}
+	}
+	return nil
+}
+
+func (l *list) tryRemove(entry *node) bool {
+	inderect := &l.head
+
+	for this := loadNode(inderect); this != entry; this = loadNode(inderect) {
+		// already deleted by another thread
+		if this == nil {
+			return false
+		}
+		inderect = &this.next
+	}
+
+	old, new := unsafe.Pointer(entry), atomic.LoadPointer(&entry.next)
+	return atomic.CompareAndSwapPointer(inderect, old, new)
+}
+
+func (l *list) Get(key string) (value listValue, ok bool) {
+	if entry := find(l.Head(), key); entry != nil {
+		return entry.value, true
+	}
+	return
+}
+
+func (l *list) Delete(key string) {
+	for entry := find(l.Head(), key); entry != nil; entry = find(l.Head(), key) {
+		if ok := l.tryRemove(entry); ok {
+			break
+		}
+	}
+}
+
 func (l *list) Insert(key string, value listValue) {
+	// push to the top
 	ptr := &node{
 		key:   key,
 		value: value,
@@ -37,18 +81,17 @@ func (l *list) Insert(key string, value listValue) {
 	new := unsafe.Pointer(ptr)
 	for {
 		old := atomic.LoadPointer(&l.head)
-		ptr.next = old
-		if atomic.CompareAndSwapPointer(&l.head, old, new) {
+		if ptr.next = old; atomic.CompareAndSwapPointer(&l.head, old, new) {
 			break
 		}
 	}
-}
 
-func (l *list) Get(key string) (value listValue, ok bool) {
-	for this := l.Head(); this != nil; this = this.Next() {
-		if this.key == key {
-			return this.value, true
+	// remove all old nodes before new one
+	for {
+		entry := find(ptr.Next(), key)
+		if entry == nil {
+			break
 		}
+		l.tryRemove(entry)
 	}
-	return
 }
